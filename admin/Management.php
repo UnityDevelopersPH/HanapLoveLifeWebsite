2f13d86e0960f3dcd75653e340b970c353a3138b
UnityDevelopersPH <elitecyberdevelopersph@gmail.com>
2020-04-03 20:55:38 +0800

    remove nav side

--- admin/Management.php
+++ admin/Management.php
@@ -91,37 +91,6 @@
         <p>Hanap LoveLife</p>
       </div>
 
-      <div class="d-flex change-mode">
-
-        <div class="ml-auto mb-0 mr-3 change-mode-wrapper">
-          <button class="btn btn-outline-black btn-sm" id="dark-mode">Change Mode</button>
-        </div>
-
-        <!-- Navbar links -->
-        <ul class="nav navbar-nav nav-flex-icons ml-auto">
-          <li class="nav-item">
-            <a class="nav-link waves-effect"><i class="fas fa-envelope"></i> <span
-                class="clearfix d-none d-sm-inline-block">Contact</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link waves-effect"><i class="far fa-comments"></i> <span
-                class="clearfix d-none d-sm-inline-block">Support</span></a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle waves-effect" href="#" id="userDropdown" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-user"></i> <span class="clearfix d-none d-sm-inline-block">Profile</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-              <a class="dropdown-item" href="#">Log Out</a>
-              <a class="dropdown-item" href="#">My account</a>
-            </div>
-          </li>
-
-        </ul>
-        <!-- Navbar links -->
-
-      </div>
 
     </nav>
     <!-- Navbar -->
