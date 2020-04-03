5bcadf0d7294448f3c3b2bc2951fade7b05a0b83
UnityDevelopersPH <elitecyberdevelopersph@gmail.com>
2020-04-03 22:57:15 +0800

    Inupload ko muna inayos ko na din ung sidenav

--- admin/UserControl.php
+++ admin/UserControl.php
@@ -23,64 +23,7 @@
   <!-- Main Navigation -->
   <header>
 
-    <!-- Sidebar navigation -->
-    <div id="slide-out" class="side-nav sn-bg-4 fixed">
-      <ul class="custom-scrollbar">
-
-        <!-- Logo -->
-        <li class="logo-sn waves-effect py-3">
-          <div class="text-center">
-            <a href="#" class="pl-0"><img src="img/logo.png">Hanap LoveLife</a>
-          </div>
-        </li>
-
-        <!-- Search Form -->
-        <li>
-          <form class="search-form" role="search">
-            <div class="md-form mt-0 waves-light">
-              <input type="text" class="form-control py-2" placeholder="Search">
-            </div>
-          </form>
-        </li>
-
-        <!-- Side navigation links -->
-        <li>
-          <ul class="collapsible collapsible-accordion">
-
-            <!-- Simple link -->
-						<li>
-              <a href="index.php" class="collapsible-header waves-effect"><i
-                  class="w-fa far fa-bell"></i>Home</a>
-            </li>
-						<li>
-              <a href="Announcement.php" class="collapsible-header waves-effect"><i
-                  class="w-fa far fa-bell"></i>Announcement</a>
-            </li>
-            <li>
-              <a href="Promotion.php" class="collapsible-header waves-effect"><i
-                  class="w-fa fas fa-bolt"></i>Promotion</a>
-            </li>
-            <li>
-              <a href="UserControl.php" class="collapsible-header waves-effect"><i
-                  class="w-fa fas fa-chart-pie"></i>User Control</a>
-            </li>
-            <li>
-              <a href="Discount.php" class="collapsible-header waves-effect"><i
-                  class="w-fa far fa-calendar-check"></i>Discount Promotion</a>
-            </li>
-            <li>
-              <a href="Management.php" class="collapsible-header waves-effect"><i
-                  class="w-fa fas fa-th-large"></i>Management</a>
-            </li>
-
-
-          </ul>
-        </li>
-        <!-- Side navigation links -->
-
-      </ul>
-      <div class="sidenav-bg mask-strong"></div>
-    </div>
+   <?php include 'include/sidenav.php'; ?>
     <!-- Sidebar navigation -->
 
     <!-- Navbar -->
@@ -100,9 +43,160 @@
     <!-- Navbar -->
 
   </header>
-  <!-- Main Navigation -->
+
 
   <!-- Main layout -->
+   <main>
+    <div class="container-fluid">
+
+      <!-- Section: Basic examples -->
+      <section>
+
+      
+
+        <!-- Top Table UI -->
+
+
+          <!-- Grid row -->
+          <div class="row">
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-lg-12 col-md-12 right">
+              <form class="form-inline md-form mt-2 ml-2">
+                <input class="form-control mt-2" type="text" placeholder="Search" style="max-width: 150px;">
+                <button class="btn btn-sm btn-primary ml-2 px-1"><i class="fas fa-search"></i> </button>
+              </form>
+
+            </div>
+            <!-- Grid column -->
+
+          </div>
+          <!-- Grid row -->
+
+
+        <!-- Top Table UI -->
+
+        <div class="card card-cascade narrower z-depth-1">
+
+          <!-- Card image -->
+          <div class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
+
+            <div>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-th-large mt-0"></i></button>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-columns mt-0"></i></button>
+            </div>
+
+            <a href="" class="white-text mx-3">Hanap LoveLife User's</a>
+
+            <div>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-pencil-alt mt-0"></i></button>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-eraser mt-0"></i></button>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-info-circle mt-0"></i></button>
+            </div>
+
+          </div>
+          <!-- /Card image -->
+
+          <div class="px-4">
+
+            <div class="table-responsive">
+              <!-- Table -->
+              <table class="table table-hover mb-0 align-middle">
+
+                <!-- Table head -->
+                <thead>
+                  <tr>
+                    <th class="th-lg"><a> Username </a></th>
+                    <th class="th-lg"><a href="">Email</a></th>
+                    <th class="th-lg"><a href="">Age</a></th>
+                    <th class="th-lg"><a href="">Gender</a></th>
+                    <th class="th-lg"><a href="">Phone Number</a></th>
+                    <th class="th-lg"><a href="">Status</a></th>
+                  </tr>
+                </thead>
+                <!-- Table head -->
+
+                <!-- Table body -->
+                <tbody>
+                  <tr>
+                    <td>hackdog</td>
+                    <td>hackdog@gmail.com</td>
+                    <td>21</td>
+                    <td>Male</td>
+                    <td>0912345678</td>
+                    <td><span class="badge badge-danger badge-lg"> Offline </span></td>
+                  </tr>
+                </tbody>
+                <!-- Table body -->
+              </table>
+              <!-- Table -->
+            </div>
+
+            <hr class="my-0">
+
+            <!-- Bottom Table UI -->
+            <div class="d-flex justify-content-between">
+
+              <!-- Name -->
+              <select class="mdb-select colorful-select dropdown-primary mt-2">
+                <option value="" disabled>Rows number</option>
+                <option value="1" selected>5 rows</option>
+                <option value="2">25 rows</option>
+                <option value="3">50 rows</option>
+                <option value="4">100 rows</option>
+              </select>
+
+              <!-- Pagination -->
+              <nav class="my-4">
+                <ul class="pagination pagination-circle pg-blue mb-0">
+
+                  <!-- First -->
+                  <li class="page-item disabled clearfix d-none d-md-block"><a class="page-link">First</a></li>
+
+                  <!-- Arrow left -->
+                  <li class="page-item disabled">
+                    <a class="page-link" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                  </li>
+
+                  <!-- Numbers -->
+                  <li class="page-item active"><a class="page-link">1</a></li>
+                  <li class="page-item"><a class="page-link">2</a></li>
+                  <li class="page-item clearfix d-none d-md-inline-block"><a class="page-link">3</a></li>
+                  <li class="page-item clearfix d-none d-md-inline-block"><a class="page-link">4</a></li>
+                  <li class="page-item clearfix d-none d-md-inline-block"><a class="page-link">5</a></li>
+
+                  <!-- Arrow right -->
+                  <li class="page-item">
+                    <a class="page-link" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </li>
+
+                  <!-- First -->
+                  <li class="page-item clearfix d-none d-md-block"><a class="page-link">Last</a></li>
+
+                </ul>
+              </nav>
+              <!-- /Pagination -->
+
+            </div>
+            <!-- Bottom Table UI -->
+
+          </div>
+        </div>
+
+      </section>
+      <!-- Section: Basic examples -->
+
+    </div>
+
+    <hr class="my-5">
+  </main>
 
   <!-- Main layout -->
 
---
+++ admin/include/sidenav.php
@@ -0,0 +1,57 @@
+    <!-- Sidebar navigation -->
+    <div id="slide-out" class="side-nav sn-bg-4 fixed">
+      <ul class="custom-scrollbar">
+
+        <!-- Logo -->
+        <li class="logo-sn waves-effect py-3">
+          <div class="text-center">
+            <a href="#" class="pl-0"><img src="img/logo.png">Hanap LoveLife</a>
+          </div>
+        </li>
+
+        <!-- Search Form -->
+        <li>
+          <form class="search-form" role="search">
+            <div class="md-form mt-0 waves-light">
+              <input type="text" class="form-control py-2" placeholder="Search">
+            </div>
+          </form>
+        </li>
+
+        <!-- Side navigation links -->
+        <li>
+          <ul class="collapsible collapsible-accordion">
+
+            <!-- Simple link -->
+            <li>
+              <a href="index.php" class="collapsible-header waves-effect"><i
+                  class="w-fa far fa-bell"></i>Home</a>
+            </li>
+            <li>
+              <a href="Announcement.php" class="collapsible-header waves-effect"><i
+                  class="w-fa far fa-bell"></i>Announcement</a>
+            </li>
+            <li>
+              <a href="Promotion.php" class="collapsible-header waves-effect"><i
+                  class="w-fa fas fa-bolt"></i>Promotion</a>
+            </li>
+            <li>
+              <a href="UserControl.php" class="collapsible-header waves-effect"><i
+                  class="w-fa fas fa-chart-pie"></i>User Control</a>
+            </li>
+            <li>
+              <a href="Discount.php" class="collapsible-header waves-effect"><i
+                  class="w-fa far fa-calendar-check"></i>Discount Promotion</a>
+            </li>
+            <li>
+              <a href="Management.php" class="collapsible-header waves-effect"><i
+                  class="w-fa fas fa-th-large"></i>Management</a>
+            </li>
+
+          </ul>
+        </li>
+        <!-- Side navigation links -->
+
+      </ul>
+      <div class="sidenav-bg mask-strong"></div>
+    </div>
